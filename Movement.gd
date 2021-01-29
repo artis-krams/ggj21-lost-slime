c3e9602884c80d37bd4d15bb45bb3be9e1b60116
artis krams <artis.krams@gmail.com>
2021-01-29 21:10:16 +0200

    animated sprites

--- Movement.gd
+++ Movement.gd
@@ -6,14 +6,23 @@ var velocity = Vector2()
 
 func get_input():
 	velocity = Vector2()
+	
+	if !Input.is_action_pressed("right") && !Input.is_action_pressed("left") && !Input.is_action_pressed("up") && !Input.is_action_pressed("down"):
+		$AnimatedSprite.play("idle")
+		
 	if Input.is_action_pressed('right'):
+		$AnimatedSprite.play("run_right")
 		velocity.x += 1
 	if Input.is_action_pressed('left'):
+		$AnimatedSprite.play("run_left")
 		velocity.x -= 1
 	if Input.is_action_pressed('down'):
+		$AnimatedSprite.play("run_up")
 		velocity.y += 1
 	if Input.is_action_pressed('up'):
+		$AnimatedSprite.play("run_down")
 		velocity.y -= 1
+		
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
