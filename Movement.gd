9a106c7a0dc0b48b728743fbca78a3434c198981
RolandsK <rolands.korsaks@gmail.com>
2021-01-30 02:00:29 +0200

    added movement sound

--- Movement.gd
+++ Movement.gd
@@ -16,21 +16,28 @@ func _process(delta):
 
 func get_input():
 	velocity = Vector2()
+	var time = $FootSteps.get_playback_position() + AudioServer.get_time_since_last_mix()
 	
 	if !Input.is_action_pressed("right") && !Input.is_action_pressed("left") && !Input.is_action_pressed("up") && !Input.is_action_pressed("down"):
 		$AnimatedSprite.play("idle")
+		$FootSteps.stop()
+		time = 0
 		
 	if Input.is_action_pressed('down'):
 		$AnimatedSprite.play("run_up")
+		$FootSteps.play(time)
 		velocity.y += 1
 	if Input.is_action_pressed('up'):
 		$AnimatedSprite.play("run_down")
+		$FootSteps.play(time)
 		velocity.y -= 1
 	if Input.is_action_pressed('right'):
 		$AnimatedSprite.play("run_right")
+		$FootSteps.play(time)
 		velocity.x += 1
 	if Input.is_action_pressed('left'):
 		$AnimatedSprite.play("run_left")
+		$FootSteps.play(time)
 		velocity.x -= 1
 		
 	velocity = velocity.normalized() * speed
