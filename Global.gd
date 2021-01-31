cd6c3f1a3f66a83b1291ff442a44d2637a01d71f
Janis Skuja <janis.skuja91@gmail.com>
2021-01-31 02:38:15 +0200

    High Score

--- Global.gd
+++ Global.gd
@@ -3,11 +3,18 @@ extends Node
 var is_game_over = false
 var is_victory = false
 
+var current_time = 0
+var high_score
+
 func reset():
 	is_game_over = false
 	is_victory = false
+	current_time = 0
 
 func game_over(victory):
 	is_victory = victory
+	if victory && !high_score || high_score > current_time:
+		high_score = current_time
+		
 	is_game_over = true
 	get_tree().change_scene('res://MainMenu.tscn')
--- MainMenu.gd
+++ MainMenu.gd
@@ -1,9 +1,16 @@
 extends Control
 
 func _ready():
+	if Global.high_score:
+		$LabelHighScore.text = 'HighScore: ' + str(Global.high_score) + ' seconds'
+	
 	if Global.is_game_over && !Global.is_victory:
+		$LabelGameOver.set_visible(true)
 		$GameOver.play()
 	elif Global.is_game_over && Global.is_victory:
+		if Global.current_time > 0:
+			$LabelYourTime.text = 'Your time: ' + str(Global.current_time) + ' seconds'
+		$LabelVictory.set_visible(true)
 		$Victory.play()
 
 func _on_BtnStart_pressed():
