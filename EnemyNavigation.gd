7cfb3fb6805dc6f9d7fe879500f6cd9e82f2930a
artis krams <artis.krams@gmail.com>
2021-01-31 16:33:03 +0200

    more mobs more danger

--- EnemyNavigation.gd
+++ EnemyNavigation.gd
@@ -3,15 +3,23 @@ extends Navigation2D
 export(float) var character_speed = 100.0
 var path = []
 var mob = preload("res://Enemy.tscn")
+var mob2 = preload("res://EnemyFlower.tscn")
 var spawnPosition = Vector2()
 onready var character = $Enemy
 
 func _ready():
 	var routePoints = get_node("Level/RoutePoints").get_children()
 	
-	var m = mob.instance()
-	add_child(m)
-	m.position = routePoints[0].position
-	m.nav = self
-	m.setRoute(routePoints)
+	for n in range(10):
+		var m = mob.instance()
+		add_child(m)
+		m.position = routePoints[n].position
+		m.nav = self
+		m.setRoute(routePoints)
 	
+	for n in range(10):
+		var m = mob2.instance()
+		add_child(m)
+		m.position = routePoints[10 + n].position
+		m.nav = self
+		m.setRoute(routePoints)
